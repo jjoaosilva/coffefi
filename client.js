033a4eaf3639a0c9910c8fa50fa54685439e4778
Allan C <acmlira@gmail.com>
2019-10-15 10:30:38 -0300

    Requisites are working with dyn codegen

--- client.js
+++ client.js
@@ -1,29 +1,30 @@
-var PROTO_PATH = __dirname + '/protos/coffe-fi.proto';
+var PROTO_PATH = __dirname + '/protos/coffefi.proto';
 
 var grpc = require('grpc');
 var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
-var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+var coffefi_proto = grpc.loadPackageDefinition(packageDefinition).coffefi;
 
 function main() {
-  var client = new hello_proto.Greeter('localhost:50051',
-                                       grpc.credentials.createInsecure());
-  var user;
-  if (process.argv.length >= 3) {
-    user = process.argv[2];
-  } else {
-    user = 'world';
-  }
-  client.sayHello({name: user}, function(err, response) {
-    console.log('Greeting:', response.message);
-  });
+    var client = new coffefi_proto.CoffeFi('localhost:50051',grpc.credentials.createInsecure());
+    
+    client.temperatureSensor(null, (err, response) => {
+        console.log('Temperatura: ', response.temperature, 'º');
+    });
+
+    client.lamp({switch: true}, (err, response) => {
+        console.log('A luz está ', response.power);
+    });
+
+    client.coffeMachine(null, (err, response) => {
+        console.log('A máquina de café está ', response.status);
+    });
 }
 
 main();
--- server.js
+++ server.js
@@ -1,33 +1,43 @@
-var PROTO_PATH = __dirname + '/protos/coffe-fi.proto';
+var PROTO_PATH = __dirname + '/protos/coffefi.proto';
 
 var grpc = require('grpc');
 var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
-var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+var coffefi_proto = grpc.loadPackageDefinition(packageDefinition).coffefi;
 
 /**
- * Implements the SayHello RPC method.
- */
-function sayHello(call, callback) {
-  callback(null, {message: 'Hello ' + call.request.name});
+* Implements the RPC methods.
+*/
+function temperatureSensor(call, callback) {
+    callback(null, {temperature: 32});
 }
 
+function lamp(call, callback) {
+    callback(null, {power: call.request.switch ? 'ON' : 'OFF'});
+}
+
+function coffeMachine(call, callback) {
+    callback(null, {status: "EM ESPERA"});
+}
 /**
- * Starts an RPC server that receives requests for the Greeter service at the
- * sample server port
- */
+* Starts an RPC server that receives requests for the Greeter service at the
+* sample server port
+*/
 function main() {
-  var server = new grpc.Server();
-  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
-  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-  server.start();
+    var server = new grpc.Server();
+    server.addService(coffefi_proto.CoffeFi.service, {
+        temperatureSensor: temperatureSensor,
+        lamp: lamp,
+        coffeMachine: coffeMachine
+    });
+    server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+    server.start();
 }
 
 main();
